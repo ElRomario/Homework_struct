297631db9da789b9308c15b209fe50b9d5613694
ElRomario <hellishassailant007@gmail.com>
2023-04-07 21:59:22 +0300

    homewroks done

--- Student_struct/Student_struct.cpp
+++ Student_struct/Student_struct.cpp
@@ -1,20 +1,132 @@
-﻿// Student_struct.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿/*Задание 1. Описать структуру Student (фамилия, группа, успеваемость
+(массив из 5 int)). Создать массив студентов и написать программу,
+позволяющую:
+● Динамически изменять размер массива
+● Выводить список отличников (>75% отличных оценок)
+● Выводить список двоечников (>50% оценок 2 и 3)
+*/
 
-#include <iostream>
+#include <iostream> 
+#include <string> 
+using namespace std;
+
+struct student
+{
+    string name;
+    string surname;
+    string group;
+    int score[5];
+    void setStudent()
+    {
+        cout << "###############################" << endl;
+        cout << "Enter name: ";
+        cin >> name;
+        cout << "Enter surname: ";
+        cin >> surname;
+        cout << "Enter group: ";
+        cin >> group;
+        for (int i = 0; i < 5; i++)
+        {
+            switch (i)
+            {
+            case 0: cout << "Enter Math score: " << endl; cin >> score[i]; if (score[i] > 100 || score[i] < 0) { cout << "incorrect score"; exit(0); }; break;
+            case 1: cout << "Enter ESL score: " << endl; cin >> score[i]; if (score[i] > 100 || score[i] < 0) { cout << "incorrect score"; exit(0); }; break;
+            case 2: cout << "Enter Language score: " << endl; cin >> score[i]; if (score[i] > 100 || score[i] < 0) { cout << "incorrect score"; exit(0); }; break;
+            case 3: cout << "Enter Chemestry score: " << endl; cin >> score[i]; if (score[i] > 100 || score[i] < 0) { cout << "incorrect score"; exit(0); }; break;
+            case 4: cout << "Enter Biology score: " << endl; cin >> score[i]; if (score[i] > 100 || score[i] < 0) { cout << "incorrect score"; exit(0); }; break;
+            }
+        }
+        cout << "###############################" << endl;
+    }
+    void print()
+    {
+        cout << "###############################" << endl;
+        cout << "Name: " << name << endl;
+        cout << "Surname: " << surname << endl;
+        cout << "Group: " << group << endl;
+        cout << "------------score-------------" << endl;
+        for (int i = 0; i < 5; i++)
+        {
+            switch (i)
+            {
+            case 0: cout << "Math score: " << score[i] << endl; break;
+            case 1: cout << "ESL score: " << score[i] << endl; break;
+            case 2: cout << "Language score: " << score[i] << endl; break;
+            case 3: cout << "Chemestry score: " << score[i] << endl; break;
+            case 4: cout << "Biology score: " << score[i] << endl; break;
+            }
+        }
+        cout << "###############################" << endl;
+    }
+};
+
+void printAllStudents(student stArray[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        stArray[i].print();
+    }
+}
+void  addNewStudent(student** arr, int& size)
+{
+    student* newArr = new student[size + 1];
+    for (int i = 0; i < size; i++)
+    {
+        newArr[i] = (*arr)[i];
+    }
+
+    cout << "-------------Enter new student data:-------------" << endl;
+    newArr[size].setStudent();
+    delete[] * arr;
+    (*arr) = newArr;
+    size++;
+}
+void printFools(student* studentArr, int size)
+{
+    cout << "----------------------Fools----------------------" << endl;
+    int foolCounter = 0;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < sizeof(studentArr[i].score) / sizeof(studentArr[i].score[j]); j++)
+        {
+            if (studentArr[i].score[j] < 50) { foolCounter++; }
+            if (foolCounter >= 2) { studentArr[i].print(); break; }
+        }
+        foolCounter = 0;
+    }
+
+}
+void printExcellentStudents(student* studentArr, int size)
+{
+    cout << "---------------Excellent-students----------------" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < sizeof(studentArr[i].score) / sizeof(studentArr[i].score[j]); j++)
+        {
+            if (studentArr[i].score[j] > 75) { studentArr[i].print(); break; }
+        }
+    }
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int size = 2;
+    student st1, st2;
+    st1.setStudent();
+    st2.setStudent();
+    st1.print();
+    st2.print();
+
+    student* studentArray = new student[2];
+    studentArray[0] = st1;
+    studentArray[1] = st2;
+
+    addNewStudent(&studentArray, size);
+    printAllStudents(studentArray, size);
+
+    printFools(studentArray, size);
+
+    delete[] studentArray;
+
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
